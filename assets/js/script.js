5bb2b5890f452957eb612ddb23be3b834fba596a
MichaelLetson <mletsongamble@gmail.com>
2022-02-05 17:30:30 +0000

    add functionality to swap between noughts and crosses per click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,28 @@ var game = "ul.game",
 gameSquare = "ul.game li.blank",
 nought = "ul.game li.nought",
 cross = "ul.game li.cross";
+
+// array for players
+var gameResult = [nought, cross];
+
+// to push square ID to array
+var squareIDs = [];
+
+// to reset the x value for every second click (1=noughts and 0=crosses)
+var x = 0;
+function resetClick() {
+	x = 0;
+	gameSquare.value = x;
+}
+
+function count() {
+	if(x<1) {
+		x += 1;
+	}
+	else {
+		x = 0;
+	}}
+
+$( "p.reset" ).click(function() {
+         location.reload(true);
+});
