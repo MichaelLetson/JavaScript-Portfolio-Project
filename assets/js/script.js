ca22190f9a64c5ed6405fe0de137ecf7875b7ad3
MichaelLetson <mletsongamble@gmail.com>
2022-02-06 12:46:58 +0000

    add and declared winning conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const PLAYERX_WON = 'PLAYERX_WON';
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
+
+	const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
