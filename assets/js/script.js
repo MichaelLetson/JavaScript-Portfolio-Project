91f7eb2028a97e02e0c7ed1f7b1e83c027e55755
MichaelLetson <mletsongamble@gmail.com>
2022-02-06 12:56:39 +0000

    include function to change DOM to announce winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,27 @@ window.addEventListener('DOMContentLoaded', () => {
                 break;
             }
         }
+
+	if (roundWon) {
+        announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+        isGameActive = false;
+        return;
+    }
+
+    if (!board.includes(''))
+        announce(TIE);
+    }
+
+	const announce = (type) => {
+        switch(type){
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                break;
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
